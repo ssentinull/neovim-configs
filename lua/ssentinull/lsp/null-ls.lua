3cc90c744ed348a50a60dd107991cea014ce32a0
Ibnu Ahsani <ibnu.muhari@gmail.com>
2023-06-17 14:04:19 +0700

    refactor: remove unused lines

--- lua/ssentinull/lsp/null-ls.lua
+++ lua/ssentinull/lsp/null-ls.lua
@@ -39,25 +39,4 @@ null_ls.setup({
 			})
 		end
 	end,
-
-	-- -- function from here: https://github.com/josean-dev/dev-environment-files/blob/b9d2822e0676633fe7f29b996937cb1ef7fbf11e/.config/nvim/lua/josean/plugins/lsp/null-ls.lua#L30
-	-- -- configure format on save
-	-- on_attach = function(current_client, bufnr)
-	--     if current_client.supports_method("textDocument/formatting") then
-	--         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-	--         vim.api.nvim_create_autocmd("BufWritePre", {
-	--             group = augroup,
-	--             buffer = bufnr,
-	--             callback = function()
-	--                 vim.lsp.buf.format({
-	--                     filter = function(client)
-	--                         --  only use null-ls for formatting instead of lsp server
-	--                         return client.name == "null-ls"
-	--                     end,
-	--                     bufnr = bufnr,
-	--                 })
-	--             end,
-	--         })
-	--     end
-	-- end,
 })
