688908656e1bcdfd48f7d1934e27400285608f8a
Ibnu Ahsani <ibnu.muhari@gmail.com>
2023-06-22 23:16:08 +0700

    refactor: set ctrl + j as move to next option in nvim-cmp

--- lua/ssentinull/plugins/nvim-cmp.lua
+++ lua/ssentinull/plugins/nvim-cmp.lua
@@ -25,7 +25,7 @@ cmp.setup({
 	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
